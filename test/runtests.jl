0162528afbee45818dc4ef41046ac5d506788382
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-05-04 09:04:44 +0200

    Added simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using DiracQC
 using Test
 
+reffile(name) = joinpath(dirname(@__FILE__), "refdata", "$(name).out")
+
 @testset "DiracQC.jl" begin
-    # Write your tests here.
+    xenon_data = DiracQC.load(reffile("hf_Xe"))
+    xenon_energies = xenon_data.energies
+    @test xenon_energies.Electronic_energy ≈ -328.3499373239049
+    @test xenon_energies.Nuclear_repulsion_energy ≈ 0 atol=1e-14
+    @test xenon_energies.Total_energy ≈ -328.3499373239049
+    
+    @test length(xenon_energies.orbitals) == 7
+    @test [o.symmetry for o in xenon_energies.orbitals] == vcat(repeat(["E1g"], 3), repeat(["E1u"], 4))
+
+    @test size(xenon_energies.orbitals_combined) == (135, 6)
 end
