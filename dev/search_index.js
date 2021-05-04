78f29e91ef05ada12109c2f09fe7def00f9e5e34
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 07:06:42 +0000

    build based on 0162528

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiracQC","category":"page"},{"location":"#DiracQC","page":"Home","title":"DiracQC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiracQC]","category":"page"}]
+}
